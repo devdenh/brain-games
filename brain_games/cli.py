8c854dbc08d08d2d262eace305c1bf99a866a104
Denis Aminov <Denisaminov74@gmail.com>
2022-02-23 19:25:04 +0500

    step3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,7 +3,5 @@ import prompt
 name = prompt.string('May I have your name? ')
 
 def welcome_user():
-        pass
-
-print(f"Hello, {name}!")
-
+        
+    print(f"Hello, {name}!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,6 +2,9 @@
 def main():
     print("Welcome to the Brain Games!")
 
+    if __name__ == '__main__':
+        main()
 
-if __name__ == '__main__':
-    main()
+import brain_games.cli
+from brain_games.cli import welcome_user
+welcome_user()
