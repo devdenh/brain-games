1a5a65fdf14e434b18a1c5cd99ea017d27b3e6a1
Denis Aminov <Denisaminov74@gmail.com>
2022-03-12 00:28:54 +0500

    add brain_prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.engine import run_game
+from brain_games.games import brain_prime_game
+
+
+def main():
+    return run_game(brain_prime_game)
+
+
+if __name__ == '__main__':
+    main()
