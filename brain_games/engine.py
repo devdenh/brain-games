26c9f37085eaf02189a296862840f73b99654750
Denis Aminov <Denisaminov74@gmail.com>
2022-03-12 15:21:40 +0500

    fix false answer try again

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -17,6 +17,6 @@ def run_game(games):
         if str(player_answer) == str(right_answer) or player_answer == right_answer:
             print('Correct!')
         else:
-            return print(f"'{player_answer}' is wrong answer ;(. "
-                         f"Correct answer was {right_answer}.")
+            return print(f"'{player_answer}' is wrong answer ;(. Correct answer was {right_answer}."
+                         f"\nLet's try again, {name}!")
     print('Congratulations, {}!'.format(name))
